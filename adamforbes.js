ee0bfa99473f954843931676b764dc41535ab9a4
Adam Forbes <aforbes@wesleyan.edu>
2015-12-07 20:29:45 -0500

    CSS Bug fixes and refactoring
    Styling the middle section better
    Solved very annoying main-page width issue

--- adamforbes.js
+++ adamforbes.js
@@ -18,7 +18,7 @@ if (goog.DEBUG) {
 
 
 adamforbes.mainPage.overlays = function(opt_data, opt_ignored) {
-  return '<button class=\'menu-button float-right\'>TEST</button><div class=\'animation-overlay\'></div><div class=\'page-too-small-overlay ts-h2\'><div class=\'page-too-small-overlay-content\'>\u00AF\\_(\u30C4)_/\u00AF</div></div><div class=\'layering-shadow-overlay\'></div>';
+  return '<button class=\'menu-button float-right\'>MENU</button><div class=\'animation-overlay\'></div><div class=\'page-too-small-overlay ts-h2\'><div class=\'page-too-small-overlay-content\'>\u00AF\\_(\u30C4)_/\u00AF</div></div><div class=\'layering-shadow-overlay\'></div>';
 };
 if (goog.DEBUG) {
   adamforbes.mainPage.overlays.soyTemplateName = 'adamforbes.mainPage.overlays';
--- js/main.js
+++ js/main.js
@@ -246,12 +246,12 @@ function animatedLoad(functionToRunWhileHidden) {
   });
 }
 
-function set_body_height() { // set body height = window height
-    $('body').height($(window).height());
-}
+// function set_body_height() { // set body height = window height
+//     $('body').height($(window).height());
+// }
 
-$(document).ready(function() {
-  $(window).bind('resize', set_body_height);
-  set_body_height();
-});
+// $(document).ready(function() {
+//   $(window).bind('resize', set_body_height);
+//   set_body_height();
+// });
 
